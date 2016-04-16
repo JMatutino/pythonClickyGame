866122b112e93c96f46d11c396108d935a252883
JMatutino <jacobmatutino@gmail.com>
2016-04-15 18:28:12 -0700

    Game now removes clicks as you play

--- clickyGame.py
+++ clickyGame.py
@@ -1,18 +1,37 @@
-import tkinter
+import tkinter, random
 
 class Game:
+  def setCounter(self):
+    if self.win:
+      self.counter.configure(text="YOU WIN!!")
+    else:
+      self.counter.configure(text="%d" % self.count)            
+
   def addCount(self):
     self.count += 1
     if self.count == 10 or self.win:
       self.win = True
-      self.counter.configure(text="YOU WIN!!")
+    self.setCounter()
+      
+  def subtractCount(self):
+    randNum = random.randrange(1,3)
+    print(randNum)
+    if self.count - randNum <= 0:
+      self.count = 0
     else:
-      self.counter.configure(text="%d" % self.count)
-
+      self.count -= randNum
+    self.setCounter() 
+    if not self.win:
+      self.root.after( 1000, self.subtractCount )
+      
+   
   def createGui(self):
     self.root = tkinter.Tk()
     self.root.geometry("150x150")
+    
     self.counter = tkinter.Label(self.root, text=self.count)
+    self.root.after( 1000, self.subtractCount )
+    
     self.button = tkinter.Button(self.root,text="clicky", command= self.addCount)
     
     self.counter.pack()
